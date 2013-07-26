c54c1f4f6400015317144ff97e906af63c5156a5
Simon Richardson <stickupkid@gmail.com>
2013-07-26 16:18:48 +0100

    Adding uglify to the build script.

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function (grunt) {
         jshint: {
             all: [
                 'Gruntfile.js',
-                'dist/squishy-pants.js',
+                'bin/squishy-pants.js',
                 'src/*.js',
                 'test/*.js'
             ]
@@ -25,12 +25,24 @@ module.exports = function (grunt) {
             all: [
                 'test/*.js'
             ]
+        },
+        uglify: {
+            all: {
+                options: {
+                    beautify: false,
+                    sourceMap: 'bin/squishy-pants.min.map.js'
+                },
+                files: {
+                    'bin/squishy-pants.min.js': ['bin/squishy-pants.js']
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-rigger');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', ['rig', 'jshint', 'nodeunit']);
+    grunt.registerTask('default', ['rig', 'jshint', 'nodeunit', 'uglify']);
 };
