7f2079d5a6d500cc4c5d7658e38856a6e3abf11b
Simon Richardson <simon@ustwo.co.uk>
2013-07-29 15:39:16 +0100

    Reverting attempt.

--- test/attempt.js
+++ test/attempt.js
@@ -1,85 +1,148 @@
-// instrument by jscoverage, do not modifly this file
-(function () {
-  var BASE;
-  if (typeof global === 'object') {
-    BASE = global;
-  } else if (typeof window === 'object') {
-    BASE = window;
-  } else {
-    throw new Error('[jscoverage] unknow ENV!');
-  }
-  if (!BASE._$jscoverage) {
-    BASE._$jscoverage = {};
-    BASE._$jscoverage_cond = {};
-    BASE._$jscoverage_done = function (file, line, express) {
-      if (arguments.length === 2) {
-        BASE._$jscoverage[file][line] ++;
-      } else {
-        BASE._$jscoverage_cond[file][line] ++;
-        return express;
-      }
-    };
-    BASE._$jscoverage_init = function (base, file, lines) {
-      var tmp = [];
-      for (var i = 0; i < lines.length; i ++) {
-        tmp[lines[i]] = 0;
-      }
-      base[file] = tmp;
-    };
-  }
-})();
-_$jscoverage_init(_$jscoverage, "test/array.js",[1,3,6,12,12,14,15,16,23,26,27,27,30,36,39,40,40,43]);
-_$jscoverage_init(_$jscoverage_cond, "test/array.js",[12,27,40]);
-_$jscoverage["test/array.js"].source = ["var _ = require('./lib/test');","","exports.array = {","    'when testing concat with array should yield merged array': _.check(","        function(a, b) {","            return _.concat(a, b).toString() === a.concat(b).toString();","        },","        [_.arrayOf(_.AnyVal), _.arrayOf(_.AnyVal)]","    ),","    'when testing exists with array should yield item': _.check(","        function(a) {","            if (a.length === 0) return true;","","            var index = Math.floor(_.randomRange(0, a.length));","            return _.exists(a, function(v) {","                return v === a[index];","            });","        },","        [_.arrayOf(_.AnyVal)]","    ),","    'when testing filter with array should yield items': _.check(","        function(a) {","            var accum = [],","                i;","","            for(i = 0; i < a.length; i++) {","                if (_.isEven(a[i])) accum.push(a[i]);","            }","","            return _.filter(a, _.isEven).toString() === accum.toString();","        },","        [_.arrayOf(Number)]","    ),","    'when testing filterNot with array should yield items': _.check(","        function(a) {","            var accum = [],","                i;","","            for(i = 0; i < a.length; i++) {","                if (_.isOdd(a[i])) accum.push(a[i]);","            }","","            return _.filterNot(a, _.isEven).toString() === accum.toString();","        },","        [_.arrayOf(Number)]","    )","};"];
-_$jscoverage_done("test/array.js", 1);
-var _ = require("./lib/test");
+var _ = require('./lib/test');
 
-_$jscoverage_done("test/array.js", 3);
-exports.array = {
-    "when testing concat with array should yield merged array": _.check(function(a, b) {
-        _$jscoverage_done("test/array.js", 6);
-        return _.concat(a, b).toString() === a.concat(b).toString();
-    }, [ _.arrayOf(_.AnyVal), _.arrayOf(_.AnyVal) ]),
-    "when testing exists with array should yield item": _.check(function(a) {
-        _$jscoverage_done("test/array.js", 12);
-        if (_$jscoverage_done("test/array.js", 12, a.length === 0)) {
-            _$jscoverage_done("test/array.js", 12);
-            return true;
-        }
-        _$jscoverage_done("test/array.js", 14);
-        var index = Math.floor(_.randomRange(0, a.length));
-        _$jscoverage_done("test/array.js", 15);
-        return _.exists(a, function(v) {
-            _$jscoverage_done("test/array.js", 16);
-            return v === a[index];
-        });
-    }, [ _.arrayOf(_.AnyVal) ]),
-    "when testing filter with array should yield items": _.check(function(a) {
-        _$jscoverage_done("test/array.js", 23);
-        var accum = [], i;
-        _$jscoverage_done("test/array.js", 26);
-        for (i = 0; i < a.length; i++) {
-            _$jscoverage_done("test/array.js", 27);
-            if (_$jscoverage_done("test/array.js", 27, _.isEven(a[i]))) {
-                _$jscoverage_done("test/array.js", 27);
-                accum.push(a[i]);
-            }
-        }
-        _$jscoverage_done("test/array.js", 30);
-        return _.filter(a, _.isEven).toString() === accum.toString();
-    }, [ _.arrayOf(Number) ]),
-    "when testing filterNot with array should yield items": _.check(function(a) {
-        _$jscoverage_done("test/array.js", 36);
-        var accum = [], i;
-        _$jscoverage_done("test/array.js", 39);
-        for (i = 0; i < a.length; i++) {
-            _$jscoverage_done("test/array.js", 40);
-            if (_$jscoverage_done("test/array.js", 40, _.isOdd(a[i]))) {
-                _$jscoverage_done("test/array.js", 40);
-                accum.push(a[i]);
-            }
-        }
-        _$jscoverage_done("test/array.js", 43);
-        return _.filterNot(a, _.isEven).toString() === accum.toString();
-    }, [ _.arrayOf(Number) ])
+exports.attempt = {
+    'when testing success with match should call success function': _.check(
+        function(a) {
+            return _.success(a).match({
+                success: function(b) {
+                    return a === b;
+                },
+                failure: _.badRight
+            });
+        },
+        [_.AnyVal]
+    ),
+    'when testing failure with match should call failure function': _.check(
+        function(a) {
+            return _.failure(a).match({
+                success: _.badLeft,
+                failure: function(b) {
+                    return a === b;
+                }
+            });
+        },
+        [String]
+    ),
+    'when creating success with value should set value on success': _.check(
+        function(a) {
+            return _.success(a).value === a;
+        },
+        [_.AnyVal]
+    ),
+    'when creating success should be valid attempt': _.check(
+        function(a) {
+            return _.isAttempt(_.success(a));
+        },
+        [_.AnyVal]
+    ),
+    'when creating success should be success': _.check(
+        function(a) {
+            return _.success(a).isSuccess;
+        },
+        [_.AnyVal]
+    ),
+    'when creating success should not be failure': _.check(
+        function(a) {
+            return !_.success(a).isFailure;
+        },
+        [_.AnyVal]
+    ),
+    'when creating success and mapping value should map to correct value': _.check(
+        function(a) {
+            return _.success(a).map(_.inc).value === a + 1;
+        },
+        [Number]
+    ),
+    'when creating success and folding should map to correct value': _.check(
+        function(a) {
+            return _.success(a).fold(
+                _.inc,
+                _.error('Failed if called')
+            ) === a + 1;
+        },
+        [Number]
+    ),
+    'when creating a failure with value should set errors on failure': _.check(
+        function(a) {
+            return _.failure(a).errors === a;
+        },
+        [_.AnyVal]
+    ),
+    'when creating failure should be valid attempt': _.check(
+        function(a) {
+            return _.isAttempt(_.failure(a));
+        },
+        [_.AnyVal]
+    ),
+    'when creating failure should be failure': _.check(
+        function(a) {
+            return _.failure(a).isFailure;
+        },
+        [_.AnyVal]
+    ),
+    'when creating failure should not be success': _.check(
+        function(a) {
+            return !_.failure(a).isSuccess;
+        },
+        [_.AnyVal]
+    ),
+    'when creating failure and mapping value should not call map': _.check(
+        function(a) {
+            return _.failure(a).map(
+                _.error('Failed if called')
+            ).errors === a;
+        },
+        [_.AnyVal]
+    ),
+    'when creating failure and folding should map to correct value': _.check(
+        function(a) {
+            return _.failure(a).fold(
+                _.error('Failed if called'),
+                _.inc
+            ) === a + 1;
+        },
+        [Number]
+    ),
+    'when creating success and calling ap with a success should concat to correct value': _.check(
+        function(a, b) {
+            return _.success(
+                _.constant(a)
+            ).ap(
+                _.success(b),
+                _.concat
+            ).value === a;
+        },
+        [_.AnyVal, _.AnyVal]
+    ),
+    'when creating success and calling ap with a failure should concat to correct value': _.check(
+        function(a, b) {
+            return _.success(
+                _.constant(a)
+            ).ap(
+                _.failure(b),
+                _.concat
+            ).errors.toString() === b.toString();
+        },
+        [_.AnyVal, _.arrayOf(_.AnyVal)]
+    ),
+    'when creating failure and calling ap with a success should concat to correct value': _.check(
+        function(a, b) {
+            return _.failure(a).ap(
+                _.success(b),
+                _.concat
+            ).errors.toString() === a.toString();
+        },
+        [_.arrayOf(_.AnyVal), _.AnyVal]
+    ),
+    'when creating failure and calling ap with a failure should concat to correct value': _.check(
+        function(a, b) {
+            var errors = _.failure(a).ap(
+                _.failure(b),
+                _.concat
+            ).errors;
+            // FIXME (Simon) : Implement equals.
+            return errors.toString() === a.concat(b).toString();
+        },
+        [_.arrayOf(_.AnyVal), _.arrayOf(_.AnyVal)]
+    )
 };
