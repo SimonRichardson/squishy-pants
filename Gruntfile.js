c9567b4dd0804cb48b9a5d554e347473c953618a
Simon Richardson <stickupkid@gmail.com>
2013-08-01 10:04:21 +0100

    Running the tests in parallel helping with dev time.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,69 +1,63 @@
 module.exports = function (grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        rig: {
-            compile: {
-                options: {
-                    banner: '/* Compiled : <%= grunt.template.today("yyyy-mm-dd HH:MM") %> */\n'
-                },
-                files: {
-                    'bin/squishy-pants.js': [
-                        'lib/rigger/rigger.js'
-                    ]
+    var fs = require('fs'),
+        path = require('path'),
+        files = fs.readdirSync('test'),
+        config = {
+            pkg: grunt.file.readJSON('package.json'),
+            rig: {
+                compile: {
+                    options: {
+                        banner: '/* Compiled : <%= grunt.template.today("yyyy-mm-dd HH:MM") %> */\n'
+                    },
+                    files: {
+                        'bin/squishy-pants.js': [
+                            'lib/rigger/rigger.js'
+                        ]
+                    }
+                }
+            },
+            jshint: {
+                all: [
+                    'Gruntfile.js',
+                    'bin/squishy-pants.js',
+                    'src/*.js',
+                    'test/*.js'
+                ]
+            },
+            nodeunit: {
+                all: [
+                    'test/*.js'
+                ]
+            },
+            uglify: {
+                all: {
+                    options: {
+                        beautify: false,
+                        sourceMap: 'bin/squishy-pants.min.map.js'
+                    },
+                    files: {
+                        'bin/squishy-pants.min.js': ['bin/squishy-pants.js']
+                    }
+                }
+            },
+            parallel: {
+                tests: {
+                    tasks: []
                 }
             }
-        },
-        jshint: {
-            all: [
-                'Gruntfile.js',
-                'bin/squishy-pants.js',
-                'src/*.js',
-                'test/*.js'
-            ]
-        },
-        nodeunit: {
-            all: [
-                'test/*.js'
-            ]
-        },
-        uglify: {
-            all: {
-                options: {
-                    beautify: false,
-                    sourceMap: 'bin/squishy-pants.min.map.js'
-                },
-                files: {
-                    'bin/squishy-pants.min.js': ['bin/squishy-pants.js']
-                }
-            }
-        },
-        parallel: {
-            array: {
-                tasks: [{
-                    cmd: 'nodeunit',
-                    args: ['test/array.js']
-                }]
-            },
-            attempt: {
-                tasks: [{
-                    cmd: 'nodeunit',
-                    args: ['test/attempt.js']
-                }]
-            },
-            builtins: {
-                tasks: [{
-                    cmd: 'nodeunit',
-                    args: ['test/builtins.js']
-                }]
-            },
-            check: {
-                tasks: [{
-                    cmd: 'nodeunit',
-                    args: ['test/check.js']
-                }]
-            }
+        };
+
+    // We should make a `par` task which does this and then calls parallel.
+    files.forEach(function(filename){
+        if (path.extname(filename) === '.js') {
+            config.parallel.tests.tasks.push({
+                cmd: 'nodeunit',
+                args: ['test/' + filename]
+            });
         }
     });
+
+    grunt.initConfig(config);
 
     grunt.loadNpmTasks('grunt-rigger');
     grunt.loadNpmTasks('grunt-contrib-jshint');
@@ -72,4 +66,5 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-parallel');
 
     grunt.registerTask('default', ['rig', 'jshint', 'nodeunit', 'uglify']);
+    grunt.registerTask('tests', ['rig', 'jshint', 'parallel', 'uglify']);
 };
