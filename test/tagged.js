d2aa7a3d83860a747246b7907297aa3becf51297
Simon Richardson <simon@ustwo.co.uk>
2013-08-10 13:07:28 +0100

    Adding multiple partial matching.

--- test/tagged.js
+++ test/tagged.js
@@ -60,5 +60,53 @@ exports.match = {
             return _.equal(result, a);
         },
         [_.AnyVal]
+    ),
+    'when using recursive multiple partial matching should return correct value': _.check(
+        function(a) {
+            var value = List.Cons(2, List.Cons(3, List.Cons(a, List.Nil))),
+                result = value.match({
+                    Cons:
+                        _.partial().method(
+                            function(a) {
+                                return a === 1;
+                            },
+                            function(a, b) {
+                                return b.match({
+                                    Cons:
+                                        _.partial().method(
+                                            _.constant(true),
+                                            _.identity
+                                        ),
+                                    Nil: _.constant(-1)
+                                });
+                            }
+                        ).method(
+                            function(a) {
+                                return a === 2;
+                            },
+                            function(a, b) {
+                                return b.match({
+                                    Cons:
+                                        _.partial().method(
+                                            function(a) {
+                                                return a === 3;
+                                            },
+                                            function(a, b) {
+                                                return b.match({
+                                                    Cons: _.identity,
+                                                    Nil: _.constant(-1)
+                                                });
+                                            }
+                                        ),
+                                    Nil: _.constant(-1)
+                                });
+                            }
+                        ),
+                    Nil: _.constant(-1)
+                });
+
+            return _.equal(result, a);
+        },
+        [_.AnyVal]
     )
 };
