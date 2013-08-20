e9222c58e3c1e00adf22635de09457ffe9b76238
Simon Richardson <stickupkid@gmail.com>
2013-08-20 12:01:54 +0100

    Updating the unit tests for array and adding documentation for array as well.

--- src/array.js
+++ src/array.js
@@ -9,11 +9,10 @@
 //  * `concat(a, b)` - Appends two array objects.
 //  * `count(a, f)` - Count the number of elements in the array which satisfy a predicate.
 //  * `drop(a, n)` - Returns the array without its n first elements. If this array has less than n elements, the empty array is returned.
-//  * `dropRight(a, n)` - Returns the array without its rightmost n elements.
+//  * `dropRight(a, n)` - Returns the array without its rightmost `n` elements.
 //  * `dropWhile(a, f)` - Returns the longest suffix of this array whose first element does not satisfy the predicate.
 //  * `exists(a, f)` - Tests the existence in this array of an element that satisfies the predicate.
 //  * `filter(a, f)` - Returns all the elements of this array that satisfy the predicate p.
-//  * `filterNot(a, f)` - Returns all the elements of this array that does not satisfy the predicate p.
 //  * `flatMap(a, f)` - Applies the given function f to each element of this array, then concatenates the results.
 //  * `flatten(a)` - Concatenate the elements of this array.
 //  * `fold(a, v, f)` - Combines the elements of this array together using the binary function f, from Left to Right, and starting with the value v.
@@ -26,17 +25,24 @@
 //  * `takeRight(n)` - Returns the rightmost n elements from this array.
 //  * `takeWhile(f)` - Returns the longest prefix of this array whose elements satisfy the predicate.
 //  * `zip(a, b)` - Returns a array formed from this array and the specified array that by associating each element of the former with the element at the same position in the latter.
+//  * `zipWithIndex(a)` -  Returns a array form from this array and a index of the value that is associated with each element index position.
 //
 
+//
+//  ### ap(b, [concat])
+//
+//  Apply a function in the applicative of each function
+//  Applicative ap(ply)
+//
 var ap = curry(function(a, b) {
     var accum = [],
-        x,
-        y,
+        x = a.length,
+        y = b.length,
         i,
         j;
 
-    for(i = 0, x = a.length; i < x; i++) {
-        for(j = 0, y = b.length; j < y; j++) {
+    for(i = 0; i < x; i++) {
+        for(j = 0; j < y; j++) {
             accum.push(a[i](b[j]));
         }
     }
@@ -44,10 +50,20 @@ var ap = curry(function(a, b) {
     return accum;
 });
 
+//
+//  ### concat(b)
+//
+//  Appends two array objects.
+//
 var concat = curry(function(a, b) {
     return a.concat(b);
 });
 
+//
+//  ### count(f)
+//
+//  Count the number of elements in the array which satisfy a predicate.
+//
 var count = curry(function(a, f) {
     var accum = 0,
         total,
@@ -62,14 +78,31 @@ var count = curry(function(a, f) {
     return accum;
 });
 
+//
+//  ### drop(f)
+//
+//  Returns the array without its n first elements. If this array has
+//  less than n elements, the empty array is returned.
+//
 var drop = curry(function(a, n) {
     return a.slice(n);
 });
 
+//
+//  ### dropRight(n)
+//
+//  Returns the array without its rightmost `n` elements.
+//
 var dropRight = curry(function(a, n) {
     return a.slice(0, -n);
 });
 
+//
+//  ### dropWhile(f)
+//
+//  Returns the longest suffix of this array whose first element
+//  does not satisfy the predicate.
+//
 var dropWhile = curry(function(a, f) {
     var total,
         i;
@@ -83,6 +116,12 @@ var dropWhile = curry(function(a, f) {
     return a.slice();
 });
 
+//
+//  ### exists(f)
+//
+//  Tests the existence in this array of an element that satisfies
+//  the predicate.
+//
 var exists = curry(function(a, f) {
     var total,
         i;
@@ -96,6 +135,11 @@ var exists = curry(function(a, f) {
     return false;
 });
 
+//
+//  ### filter(f)
+//
+//  Returns all the elements of this array that satisfy the predicate p.
+//
 var filter = curry(function(a, f) {
     var accum = [],
         total,
@@ -110,6 +154,12 @@ var filter = curry(function(a, f) {
     return accum;
 });
 
+//
+//  ### flatMap(f)
+//
+//  Applies the given function f to each element of this array, then
+//  concatenates the results.
+//
 var flatMap = curry(function(a, f) {
     var accum = [],
         total,
@@ -122,6 +172,12 @@ var flatMap = curry(function(a, f) {
     return accum;
 });
 
+//
+//  ### fold(v, f)
+//
+//  Combines the elements of this array together using the binary function f,
+//  from Left to Right, and starting with the value v.
+//
 var fold = curry(function(a, v, f) {
     var total,
         i;
@@ -133,6 +189,12 @@ var fold = curry(function(a, v, f) {
     return v;
 });
 
+//
+//  ### foldRight(v, f)
+//
+//  Combines the elements of this array together using the binary function f,
+//  from Right to Left, and starting with the value v.
+//
 var foldRight = curry(function(a, v, f) {
     var i;
 
@@ -143,6 +205,12 @@ var foldRight = curry(function(a, v, f) {
     return v;
 });
 
+//
+//  ### map(f)
+//
+//  Returns the array resulting from applying the given function f to each
+//  element of this array.
+//
 var map = curry(function(a, f) {
     var accum = [],
         total,
@@ -155,6 +223,11 @@ var map = curry(function(a, f) {
     return accum;
 });
 
+//
+//  ### partition(f)
+//
+//  Partition the array in two sub-arrays according to a predicate.
+//
 var partition = curry(function(a, f) {
     var left = [],
         right = [],
@@ -169,40 +242,66 @@ var partition = curry(function(a, f) {
     return Tuple2(left, right);
 });
 
+//
+//  ### reduce(f)
+//
+//  Combines the elements of this array together using the binary operator
+//  op, from Left to Right
+//
 var reduce = curry(function(a, f) {
     var total = a.length,
-        i,
-        v;
+        v = a[0],
+        i;
 
-    for (i = 0; i < total; i++) {
-        if (i === 0) v = a[i];
-        else v = f(v, a[i]);
+    for (i = 1; i < total; i++) {
+        v = f(v, a[i]);
     }
 
     return v;
 });
 
+//
+//  ### reduceRight(f)
+//
+//  Combines the elements of this array together using the binary operator
+//  op, from Right to Left
+//
 var reduceRight = curry(function(a, f) {
     var total = a.length,
-        i,
-        v;
+        v = a[total - 1],
+        i;
 
-    for (i = total - 1; i >= 0; --i) {
-        if (i === 0) v = a[i];
-        else v = f(v, a[i]);
+    for (i = total - 2; i >= 0; --i) {
+        v = f(v, a[i]);
     }
 
     return v;
 });
 
+//
+//  ### take(v, f)
+//
+//  Returns the n first elements of this array.
+//
 var take = curry(function(a, m) {
     return a.slice(0, m);
 });
 
-var takeRight = curry(function(a, m) {
-    return a.slice(-m);
+//
+//  ### takeRight(n)
+//
+//  Returns the rightmost n elements from this array.
+//
+var takeRight = curry(function(a, n) {
+    return a.slice(-n);
 });
 
+//
+//  ### takeWhile(v, f)
+//
+//  Returns the longest prefix of this array whose elements satisfy
+//  the predicate.
+//
 var takeWhile = curry(function(a, f) {
     var total,
         i;
@@ -216,6 +315,13 @@ var takeWhile = curry(function(a, f) {
     return [];
 });
 
+//
+//  ### zip(b)
+//
+//  Returns a array formed from this array and the specified array that
+//  by associating each element of the former with the element at the same
+//  position in the latter.
+//
 var zip = curry(function(a, b) {
     var accum = [],
         total = Math.min(a.length, b.length),
@@ -228,6 +334,12 @@ var zip = curry(function(a, b) {
     return accum;
 });
 
+//
+//  ### zipWithIndex()
+//
+//  Returns a array form from this array and a index of the value that
+//  is associated with each element index position.
+//
 var zipWithIndex = curry(function(a) {
     var accum = [],
         total = a.length,
--- test/array.js
+++ test/array.js
@@ -1,9 +1,24 @@
 var _ = require('./lib/test');
 
 exports.array = {
+    'when testing ap with array should yield correct value': _.check(
+        function(a) {
+            var expected = _.fold(
+                    _.range(0, a.length),
+                    [],
+                    function(x) {
+                        return x.concat(a);
+                    }
+                ),
+                concat = _.fill(a.length)(_.constant(_.identity));
+
+            return _.expect(_.ap(concat, a)).toBe(expected);
+        },
+        [_.arrayOf(_.AnyVal)]
+    ),
     'when testing concat with array should yield merged array': _.check(
         function(a, b) {
-            return _.equal(_.concat(a, b), a.concat(b));
+            return _.expect(_.concat(a, b)).toBe(a.concat(b));
         },
         [_.arrayOf(_.AnyVal), _.arrayOf(_.AnyVal)]
     ),
@@ -16,21 +31,21 @@ exports.array = {
                 if (_.isEven(a[i])) accum = _.inc(accum);
             }
 
-            return _.equal(_.count(a, _.isEven), accum);
+            return _.expect(_.count(a, _.isEven)).toBe(accum);
         },
         [_.arrayOf(_.Integer)]
     ),
     'when testing drop with array should be correct value': _.check(
         function(a) {
             var x = Math.floor(_.randomRange(0, a.length));
-            return _.equal(_.drop(a, x), a.slice(x));
+            return _.expect(_.drop(a, x)).toBe(a.slice(x));
         },
         [_.arrayOf(_.Integer)]
     ),
     'when testing dropRight with array should be correct value': _.check(
         function(a, b) {
             var x = _.randomRange(1, a.length - 1);
-            return _.equal(_.dropRight(a, x), a.slice(0, -x));
+            return _.expect(_.dropRight(a, x)).toBe(a.slice(0, -x));
         },
         [_.arrayOf(_.Integer)]
     ),
@@ -45,7 +60,7 @@ exports.array = {
                 };
                 return rec(0);
             }
-            return _.equal(_.dropWhile(a, _.isEven), dropWhile(a, _.isEven));
+            return _.expect(_.dropWhile(a, _.isEven)).toBe(dropWhile(a, _.isEven));
         },
         [_.arrayOf(_.Integer)]
     ),
@@ -69,7 +84,7 @@ exports.array = {
                 if (_.isEven(a[i])) accum.push(a[i]);
             }
 
-            return _.equal(_.filter(a, _.isEven), accum);
+            return _.expect(_.filter(a, _.isEven)).toBe(accum);
         },
         [_.arrayOf(Number)]
     ),
@@ -82,7 +97,7 @@ exports.array = {
                 accum = accum.concat(a[i]);
             }
 
-            return _.equal(_.flatMap(a, _.identity), accum);
+            return _.expect(_.flatMap(a, _.identity)).toBe(accum);
         },
         [_.arrayOf(_.arrayOf(_.AnyVal))]
     ),
@@ -98,7 +113,7 @@ exports.array = {
                 accum = sum(accum, a[i]);
             }
 
-            return _.equal(_.fold(a, 0, sum), accum);
+            return _.expect(_.fold(a, 0, sum)).toBe(accum);
         },
         [_.arrayOf(Number)]
     ),
@@ -114,8 +129,124 @@ exports.array = {
                 accum = minus(accum, a[i]);
             }
 
-            return _.equal(_.foldRight(a, 0, minus), accum);
+            return _.expect(_.foldRight(a, 0, minus)).toBe(accum);
         },
         [_.arrayOf(Number)]
+    ),
+    'when testing map with array should yield items': _.check(
+        function(a) {
+            var accum = [],
+                i;
+
+            for(i = 0; i < a.length; i++) {
+                accum.push(_.inc(a[i]));
+            }
+
+            return _.expect(_.map(a, _.inc)).toBe(accum);
+        },
+        [_.arrayOf(Number)]
+    ),
+    'when testing partition with array should yield items': _.check(
+        function(a) {
+            var left = [],
+                right = [],
+                i;
+
+            for(i = 0; i < a.length; i++) {
+                if (_.isEven(a[i])) left.push(a[i]);
+                else right.push(a[i]);
+            }
+
+            return _.expect(_.partition(a, _.isEven)).toBe(_.Tuple2(left, right));
+        },
+        [_.arrayOf(Number)]
+    ),
+    'when testing reduce with array should yield items': _.check(
+        function(a) {
+            var accum = a[0],
+                sum = function(a, b) {
+                    return a + b;
+                },
+                i;
+
+            for(i = 1; i < a.length; i++) {
+                accum = sum(accum, a[i]);
+            }
+
+            return _.expect(_.reduce(a, sum)).toBe(accum);
+        },
+        [_.arrayOf(Number)]
+    ),
+    'when testing reduceRight with array should yield items': _.check(
+        function(a) {
+            var accum = a[a.length - 1],
+                minus = function(a, b) {
+                    return a - b;
+                },
+                i;
+
+            for (i = a.length - 2; i >= 0; --i) {
+                accum = minus(accum, a[i]);
+            }
+
+            return _.expect(_.reduceRight(a, minus)).toBe(accum);
+        },
+        [_.arrayOf(Number)]
+    ),
+    'when testing take with array should yield items': _.check(
+        function(a) {
+            var rnd = Math.floor(_.randomRange(0, a.length));
+            return _.expect(_.take(a, rnd)).toBe(a.slice(0, rnd));
+        },
+        [_.arrayOf(_.AnyVal)]
+    ),
+    'when testing takeRight with array should yield items': _.check(
+        function(a) {
+            var rnd = Math.floor(_.randomRange(0, a.length));
+            return _.expect(_.takeRight(a, rnd)).toBe(a.slice(-rnd));
+        },
+        [_.arrayOf(_.AnyVal)]
+    ),
+    'when testing takeWhile with array should yield items': _.check(
+        function(a) {
+            var accum = [],
+                i;
+
+            for(i = 0; i < a.length; i++) {
+                if (!_.isEven(a[i])) {
+                    accum = a.slice(0, i);
+                    break;
+                }
+            }
+
+            return _.expect(_.takeWhile(a, _.isEven)).toBe(accum);
+        },
+        [_.arrayOf(_.AnyVal)]
+    ),
+    'when testing zip with array should yield items': _.check(
+        function(a, b) {
+            var accum = [],
+                i;
+
+            for(i = 0; i < Math.min(a.length, b.length); i++) {
+                accum.push(_.Tuple2(a[i], b[i]));
+            }
+
+            return _.expect(_.zip(a, b)).toBe(accum);
+        },
+        [_.arrayOf(_.AnyVal), _.arrayOf(_.AnyVal)]
+    ),
+    'when testing zipWithIndex with array should yield items': _.check(
+        function(a, b) {
+            var accum = [],
+                i;
+
+            for(i = 0; i < a.length; i++) {
+                accum.push(_.Tuple2(a[i], i));
+            }
+
+            return _.expect(_.zipWithIndex(a)).toBe(accum);
+        },
+        [_.arrayOf(_.AnyVal), _.arrayOf(_.AnyVal)]
     )
 };
