3290cab3ce66d868d294c44f53336a8f2b46cbf4
Simon Richardson <stickupkid@gmail.com>
2013-09-04 12:14:42 +0100

    Implementing non destructive mixins - Adding unit tests to make sure that we can create non destructive mixins.

--- test/squishy.js
+++ test/squishy.js
@@ -1,4 +1,25 @@
 var _ = require('./lib/test');
+    A = function() {},
+    B = function() {},
+    C = function() {};
+
+A.prototype =  {
+    doA: function() {
+        return 'A';
+    }
+};
+
+B.prototype =  {
+    doB: function() {
+        return 'B';
+    }
+};
+
+C.prototype =  {
+    doC: function() {
+        return 'C';
+    }
+};
 
 exports.functionName = {
     testNamed: function(test) {
@@ -112,12 +133,45 @@ exports.singleton = {
 };
 
 exports.extend = {
-    testExtend: function(test) {
+    'when testing extend with object should create a new object': function(test) {
         test.deepEqual(_.extend({a: 1}, {b: 2}), {a: 1, b: 2});
         test.done();
     },
-    testExtendOverwrite: function(test) {
+    'when testing extend with object should create a new object without modifying new object': function(test) {
+        var a = {a: 1},
+            b = {b: 2},
+            c = _.extend(a, b);
+
+        test.deepEqual(a, {a: 1});
+        test.deepEqual(b, {b: 2});
+        test.done();
+    },
+    'when extending a a object should override property correctly from the right biased': function(test) {
         test.deepEqual(_.extend({a: 1}, {a: 2}), {a: 2});
+        test.done();
+    },
+    'when creating mixin should create a new object with all correct methods': function(test) {
+        var X = function() {},
+            x;
+
+        X.prototype = _.extend(_.extend(A.prototype, B.prototype), C.prototype);
+
+        x = new X();
+
+        test.equal(x.doA() + x.doB() + x.doC(), 'ABC');
+        test.done();
+    },
+    'when creating mixin should not modify original objects': function(test) {
+        var X = function() {},
+            x;
+
+        X.prototype = _.extend(_.extend(A.prototype, B.prototype), C.prototype);
+
+        x = new X();
+
+        test.deepEqual(A.prototype, {doA: A.prototype.doA});
+        test.deepEqual(B.prototype, {doB: B.prototype.doB});
+        test.deepEqual(C.prototype, {doC: C.prototype.doC});
         test.done();
     }
 };
