c71f70199a748e2a32e2a7fad0ed903c0c8abf3c
Simon Richardson <simon@ustwo.co.uk>
2013-09-28 16:02:38 +0100

    Starting to parse correctly

--- src/parser.js
+++ src/parser.js
@@ -2,36 +2,58 @@ var Parser = tagged('Parser', ['run']);
 
 Parser.of = function(a) {
     return Parser(function() {
+        return Tuple2(a, 0);
+    });
+};
+
+Parser.put = function(a) {
+    return Parser(function() {
         return a;
     });
 };
 
 Parser.regexp = function(r) {
     return Parser(function(stream, index) {
-        var match = r.exec(stream.slice(index));
-        return match[0];
+        var match = r.exec(stream.slice(index)),
+            result = match[0];
+
+        return Tuple2(result, index + result.length);
     });
 };
 
 Parser.prototype.chain = function(f) {
     var env = this;
-    return Parser(function(e) {
-        return f(env.run(e)).run(e);
+    return Parser(function(stream, index) {
+        var a = env.run(stream, index),
+            b = f(a._1, a._2);
+
+        return b.run(a._1, a._2);
     });
 };
 
 Parser.prototype.map = function(f) {
-    return this.chain(function(a) {
-        return Parser.of(f(a));
+    return this.chain(function(stream, index) {
+        return Parser.put(Tuple2(f(stream), index));
     });
 };
 
-Parser.prototype.orElse = function(f) {
-    var env = this;
-    return Parser(function(e) {
-        return f(e);
+Parser.prototype.skip = function(a) {
+    return this.chain(function(stream, index) {
+        return a.run(stream, index);
     });
 };
+
+Parser.prototype.parse = function(a) {
+    return this.skip(eof).run(a, 0)._1;
+};
+
+var eof = Parser(function(stream, index) {
+    return Parser(function() {
+        var result = index < stream.length ? stream : null;
+
+        return Tuple2(result, index);
+    });
+});
 
 //
 //  ## isParser(a)
--- test/parser.js
+++ test/parser.js
@@ -4,11 +4,19 @@ function toInt(a) {
     return ~~a;
 }
 
+function toFloat(a) {
+    return a.toFixed(2);
+}
+
 exports.parser = {
     'test': function(test) {
 
-        var parser = _.Parser.regexp(/^[0-9\\.]+/);
-        var result = parser.map(toInt).run('1.1');
+        var whitespace = _.Parser.regexp(/^\s+/);
+
+        var parser = _.Parser.regexp(/^-?[0-9\\.]+/);
+
+        var number = parser.map(toInt).map(toFloat);
+        var result = number.parse('1.1 2.2');
 
         console.log(result);
 
