b847fbaa6d73bb582e6f63e8793a20a1ab23710a
Simon Richardson <simon@ustwo.co.uk>
2013-09-30 12:32:24 +0100

    Improving the test for parse '(x)'

--- test/parser.js
+++ test/parser.js
@@ -1,4 +1,8 @@
-var _ = require('./lib/test');
+var _ = require('./lib/test'),
+    number = _.Parser.regexp(/^[\+\-]?\d+(\.\d+)?/),
+    whitespace = _.Parser.regexp(/^\s+/),
+    leftBracket = _.Parser.string('('),
+    rightBracket = _.Parser.string(')');
 
 function toInt(a) {
     return ~~a;
@@ -9,26 +13,19 @@ function toFloat(a) {
 }
 
 exports.parser = {
-    'test': function(test) {
+    'when testing a number in some brackets should return correct value': _.check(
+        function(a) {
+            var round = number.map(toInt).map(toFloat),
+                parser = leftBracket.skip(whitespace).chain(function(a, b, c, d) {
+                    return round.skip(whitespace).chain(function(a, b, c, d) {
+                        return rightBracket;
+                    });
+                }),
+                value = '(' + a + ')',
+                expected = toFloat(toInt(a)).toString();
 
-        var whitespace = _.Parser.regexp(/^\s+/);
-        var semiColon = _.Parser.regexp(/^;/);
-
-        var float = _.Parser.regexp(/^[\+\-]?[0-9\\.]+/);
-        var char = _.Parser.regexp(/^[a-z]/);
-        var openBracket = _.Parser.string('(');
-        var closeBracket = _.Parser.string(')');
-        var one = _.Parser.string('1');
-
-        var floatRound = float.map(toInt).map(toFloat);
-
-        var result = openBracket.skip(whitespace).chain(function(a, b, c, d) {
-            return floatRound.skip(whitespace).chain(function(a, b, c, d) {
-                return closeBracket;
-            });
-        });
-
-        test.ok(_.expect(result.parse('( 1 )')).toBe(_.Attempt.of(['(', '1.00', ')'])));
-        test.done();
-    }
+            return _.expect(parser.parse(value)).toBe(_.Attempt.of(['(', expected, ')']));
+        },
+        [Number]
+    )
 };
