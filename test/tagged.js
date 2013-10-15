b2db09dfbaaf6ac40973367ab157d18a813d4ead
Simon Richardson <stickupkid@gmail.com>
2013-10-15 11:02:22 +0100

    Adding more tests for tagged.

--- test/tagged.js
+++ test/tagged.js
@@ -24,3 +24,54 @@ exports.tagged = {
         [String, Number, Number]
     )
 };
+
+exports.taggedSum = {
+    'when checking head value should return correct value': _.check(
+        function(a) {
+            var list = List.Cons(a, List.Nil);
+
+            return _.expect(list.head).toBe(a);
+        },
+        [Number]
+    ),
+    'when checking tail value should return correct value': _.check(
+        function(a) {
+            var list = List.Cons(a, List.Nil);
+
+            return _.expect(list.tail).toBe(List.Nil);
+        },
+        [Number]
+    ),
+    'when checking match with first tagged value should return correct value': _.check(
+        function(a) {
+            var list = List.Cons(a, List.Nil),
+                actual = list.match({
+                    Cons: _.identity,
+                    Nil: _.error('Failed if called')
+                });
+
+            return _.expect(actual).toBe(a);
+        },
+        [Number]
+    ),
+    'when checking match with second tagged value should return correct value': _.check(
+        function(a) {
+            var list = List.Nil,
+                actual = list.match({
+                    Cons: _.error('Failed if called'),
+                    Nil: _.constant('nil')
+                });
+
+            return _.expect(actual).toBe('nil');
+        },
+        [Number]
+    ),
+    'when checking toString should return correct value': _.check(
+        function(a, b) {
+            var actual = List.Cons(a, List.Cons(b, List.Nil));
+
+            return _.expect(actual.toString()).toBe('Cons(' + a + ', Cons(' + b + ', Nil))');
+        },
+        [Number, Number]
+    )
+};
