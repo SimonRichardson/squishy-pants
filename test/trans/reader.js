c9e874d854c2fa61dfa02633347c3a8e7a26757b
Simon Richardson <stickupkid@gmail.com>
2013-10-25 18:17:41 +0100

    Adding more coverage of the reader.

--- test/trans/reader.js
+++ test/trans/reader.js
@@ -1,16 +1,17 @@
 var _ = require('./../lib/test');
 
 exports.readerT = {
-    /*'when testing ReaderT map should return correct value': _.check(
-        function(a, b) {
-            var transformer = _.Reader.ReaderT(_.Reader),
-                actual = transformer(_.Reader.of(a)).map(_.inc),
-                expected = transformer(_.Reader.of(a + 1));
+    'when testing ReaderT map should return correct value': _.check(
+        function(a, b, c) {
+            var x = _.Reader.ReaderT(_.Reader),
+                y = x.of(a).map(function(v) {
+                    return _.Reader.of(v + 1);
+                });
 
-            return _.expect(actual.run.run(b)).toBe(expected.run.run(b));
+            return _.expect(y.run(c).run(b).run()).toBe(a + 1);
         },
-        [Number, Number]
-    ),*/
+        [_.Integer, _.AnyVal, _.AnyVal]
+    ),
     'when testing ReaderT chain should return correct value': _.check(
         function(a, b, c) {
             var x = _.Reader.ReaderT(_.Reader),
@@ -21,44 +22,11 @@ exports.readerT = {
             return _.expect(y.run(c).run(b).run()).toBe(b);
         },
         [_.AnyVal, _.AnyVal, _.AnyVal]
-    )/*,
-    'when creating a ReaderT and using chain should be correct value': _.check(
-        function(a, b, c) {
-            var transformer = _.Reader.ReaderT(_.Reader),
-                actual = transformer(_.Reader.of(b)).chain(function(x) {
-                    return transformer.of(x + 1);
-                }),
-                expected = transformer.of(b + 1);
-
-            return _.expect(actual.run.run(c)).toBe(expected.run.run(c));
-        },
-        [Number, Number, Number]
     ),
-    'when creating a ReaderT using readerTOf and chain should be correct value': _.check(
-        function(a, b, c) {
-            var transformer = _.Reader.ReaderT(_.Reader),
-                actual = a(_.Reader.of(b)).chain(function(x) {
-                    return transformer.of(x + 1);
-                }),
-                expected = transformer.of(b + 1);
-
-            return _.expect(actual.run.run(c)).toBe(expected.run.run(c));
+    'when testing ReaderT arb should return correct value': _.check(
+        function(a, b) {
+            return _.expect(a(b).run).toBe(b);
         },
-        [_.readerTOf(Number), Number, Number]
-    ),
-    'when creating a ReaderT using readerTOf and map should be correct value': _.check(
-        function(a, b, c) {
-            var transformer = _.Reader.ReaderT(_.Reader),
-                actual = _.map(
-                    a(_.Reader.of(b)),
-                    function(x) {
-                        return x + 1;
-                    }
-                ),
-                expected = transformer.of(b + 1);
-
-            return _.expect(actual.run.run(c)).toBe(expected.run.run(c));
-        },
-        [_.readerTOf(Number), Number, Number]
-    )*/
+        [_.readerTOf(_.readerOf(_.AnyVal)), _.AnyVal]
+    )
 };
